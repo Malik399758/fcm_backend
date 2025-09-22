5964f03edfc9d71617be1549cb51a3b0910cd3fc
Arslan Qazi <arslanqazi1999@gmail.com>
2025-09-22 10:36:12 +0500

    Update app_colors.dart

--- lib/src/components/app_colors_images/app_colors.dart
+++ lib/src/components/app_colors_images/app_colors.dart
@@ -8,8 +8,8 @@ class AppColors {
   static final Color orangeColor = Color(0XFFFF6F61);
   static final Color blackColor = Color(0XFF242424);
   static final Color greyColor = Color(0XFF797979);
+  static final Color lightGrey = Color(0XFFF4F6F9);
   static final Color transparentColor = Colors.transparent;
   static final Color whiteColor = Colors.white;
-  static final Color lightGrey = Color(0XFFF4F6F9);
 
 }
