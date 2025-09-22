a10014fb8edf4a4b1dffd70801273338a34d0b7c
Arslan Qazi <arslanqazi1999@gmail.com>
2025-09-22 10:39:14 +0500

    s

--- lib/src/components/app_colors_images/app_images.dart
+++ lib/src/components/app_colors_images/app_images.dart
@@ -1,4 +1,7 @@
 class AppImages {
 
   static final String logo = 'assets/logo.svg';
+  static final String onBoarding1 = 'assets/onBoarding1.svg';
+  static final String onBoarding2 = 'assets/onBoarding2.svg';
+  static final String onBoarding3 = 'assets/onBoarding3.svg';
 }
