abecbcb8a2fabf56ad133a93c34c34389570edfa
Arslan Qazi <arslanqazi1999@gmail.com>
2025-09-23 14:38:46 +0500

    Update chat_screen.dart

--- lib/src/screen/bottom_nav_screens/home_nav_screens/chat_screen.dart
+++ lib/src/screen/bottom_nav_screens/home_nav_screens/chat_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:loneliness/src/components/app_colors_images/app_colors.dart';
+import 'package:loneliness/src/components/app_colors_images/app_images.dart';
+import 'package:loneliness/src/components/common_widget/black_text.dart';
 import 'package:loneliness/src/components/common_widget/custom_back_button.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -18,13 +20,34 @@ class ChatScreen extends StatelessWidget {
           child: Row(children: [
 
             CustomBackButton(color: AppColors.whiteColor,borderColor: AppColors.transparentColor),
-            SizedBox(width: screenWidth*.03),
-            
+            SizedBox(width: screenWidth*.05),
+            Row(children: [
+              CircleAvatar(
+                radius: screenWidth*.07,
+                backgroundImage: AssetImage(AppImages.user1),
+              ),
+              SizedBox(width: screenWidth*.03),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  BlackText(text: "Albert Flores",fontWeight: FontWeight.w500,fontSize: 14,textColor: AppColors.whiteColor,),
+                  BlackText(text: "Online",fontWeight: FontWeight.w400,fontSize: 12,textColor: AppColors.whiteColor,),
+              ],)
 
+            ],),
           ],),
+        ),
+        Container(
+          height: screenHeight*.84,
+          width: screenWidth,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30),
+              topRight: Radius.circular(30),
+            )
+          ),
         )
-
-
       ],),),
     );
   }
