dbbdc5a7510c883e052b4e89f1fce7ae219f9aca
Arslan Qazi <arslanqazi1999@gmail.com>
2025-09-23 12:54:57 +0500

    Update notification_screen.dart

--- lib/src/screen/bottom_nav_screens/home_nav_screens/notification_screen.dart
+++ lib/src/screen/bottom_nav_screens/home_nav_screens/notification_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:loneliness/src/components/app_colors_images/app_colors.dart';
+import 'package:loneliness/src/components/app_colors_images/app_images.dart';
 import 'package:loneliness/src/components/common_widget/black_text.dart';
 import 'package:loneliness/src/components/common_widget/custom_back_button.dart';
 
@@ -35,6 +38,32 @@ class NotificationScreen extends StatelessWidget {
               fontSize: 16,
             ),
               SizedBox(height: screenHeight*.03),
+              NotificationWidget(
+                  title: "New Video from Your Family!",
+                  time: "2 m ago",
+                  description: "just shared a new video. Open the app to watch and stay connected!"
+              ),
+              NotificationWidget(
+                  title: "Stay in Touch",
+                  time: "2 m ago",
+                  description: "Don’t miss out on your family’s updates. Check your messages today!"
+              ),
+              BlackText(
+                text: "Yesterday",
+                fontWeight: FontWeight.w500,
+                fontSize: 16,
+              ),
+              SizedBox(height: screenHeight*.03),
+              NotificationWidget(
+                  title: "New Video from Your Family!",
+                  time: "2 m ago",
+                  description: "just shared a new video. Open the app to watch and stay connected!"
+              ),
+              NotificationWidget(
+                  title: "Stay in Touch",
+                  time: "2 m ago",
+                  description: "Don’t miss out on your family’s updates. Check your messages today!"
+              ),
 
           ],),
         ),
@@ -42,3 +71,92 @@ class NotificationScreen extends StatelessWidget {
     );
   }
 }
+
+class NotificationWidget extends StatelessWidget {
+
+  final String title;
+  final String description;
+  final String time;
+
+  const NotificationWidget({super.key,
+    required this.title,
+    required this.time,
+    required this.description,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.sizeOf(context).width;
+    final screenHeight = MediaQuery.sizeOf(context).height;
+    return GestureDetector(
+      child: Container(
+        width: screenWidth,
+        height: screenHeight*.17,
+        padding: EdgeInsets.all(screenWidth*.04),
+        margin: EdgeInsets.only(bottom: screenHeight*.03),
+        decoration: BoxDecoration(
+          color: AppColors.greenColor,
+          borderRadius: BorderRadius.circular(15),
+          border: Border.all(color: AppColors.transparentColor),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+
+
+            Container(
+              height: screenHeight*.056,
+              width: screenWidth*.13,
+              decoration: BoxDecoration(
+                color: AppColors.whiteColor,
+                shape: BoxShape.circle,
+                border: Border.all(color: AppColors.transparentColor),
+              ),
+              child: Center(child: SvgPicture.asset(AppImages.bell,color: AppColors.greenColor,width: screenWidth*.08,),),
+            ),
+
+            SizedBox(width: screenWidth*.03),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        BlackText(
+                          text: title,
+                          textColor: AppColors.whiteColor,
+                          textAlign: TextAlign.start,
+                        ),
+                        SizedBox(height: screenHeight*.01),
+                        BlackText(
+                          text: description,
+                          textColor: AppColors.whiteColor,
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400,
+                          textAlign: TextAlign.start,
+                        ),
+                      ],
+                    ),
+                  ),
+
+                ],
+              ),
+            ),
+            SizedBox(width: screenWidth*.01),
+            BlackText(
+              text: time,
+              textColor: AppColors.whiteColor,
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+            ),
+
+          ],),
+      ),
+    );
+  }
+}
+
