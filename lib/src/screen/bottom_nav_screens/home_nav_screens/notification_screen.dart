bc8c0d22ae9ab3308ea9cba08a9a4a115a123955
Arslan Qazi <arslanqazi1999@gmail.com>
2025-09-23 12:18:35 +0500

    Update notification_screen.dart

--- lib/src/screen/bottom_nav_screens/home_nav_screens/notification_screen.dart
+++ lib/src/screen/bottom_nav_screens/home_nav_screens/notification_screen.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:loneliness/src/components/common_widget/black_text.dart';
+import 'package:loneliness/src/components/common_widget/custom_back_button.dart';
 
 class NotificationScreen extends StatelessWidget {
   const NotificationScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final screenWidth = MediaQuery.sizeOf(context).width;
+    final screenHeight = MediaQuery.sizeOf(context).height;
+    return Scaffold(
+      body: SafeArea(child: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: screenWidth*.05),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+
+            SizedBox(height: screenHeight*.03),
+            Row(children: [
+              CustomBackButton(),
+              Spacer(),
+              BlackText(
+                text: "Notification",
+                fontWeight: FontWeight.w600,
+                fontSize: 16,
+              ),
+              Spacer(),
+            ],),
+            SizedBox(height: screenHeight*.05),
+            BlackText(
+              text: "Today",
+              fontWeight: FontWeight.w500,
+              fontSize: 16,
+            ),
+              SizedBox(height: screenHeight*.03),
+
+          ],),
+        ),
+      )),
+    );
   }
 }
