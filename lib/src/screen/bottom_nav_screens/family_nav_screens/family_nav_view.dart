6a1cbe862b3448125adfb66ddd51cfb36e88cf86
Arslan Qazi <arslanqazi1999@gmail.com>
2025-09-24 09:28:51 +0500

    Update family_nav_view.dart

--- lib/src/screen/bottom_nav_screens/family_nav_screens/family_nav_view.dart
+++ lib/src/screen/bottom_nav_screens/family_nav_screens/family_nav_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:loneliness/src/components/app_colors_images/app_colors.dart';
+import 'package:loneliness/src/components/common_widget/black_text.dart';
 
 class FamilyNavView extends StatelessWidget {
   const FamilyNavView({super.key});
@@ -11,8 +13,38 @@ class FamilyNavView extends StatelessWidget {
       body: SafeArea(child: SingleChildScrollView(
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: screenWidth*.05),
-          child: Column(children: [
-            
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: screenHeight*.05),
+              BlackText(
+                text: "My Families",
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+              SizedBox(height: screenHeight*.006),
+              BlackText(
+                text: "Create Group",
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                textColor: AppColors.greyColor,
+              ),
+              SizedBox(height: screenHeight*.03),
+              BlackText(
+                text: "Family members",
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+              SizedBox(height: screenHeight*.03),
+              Container(
+                width: screenWidth,
+                color: Colors.red,
+                height: 80,
+                child: Row(children: [
+
+                  
+                ],),
+              )
           ],),
         ),
       )),
