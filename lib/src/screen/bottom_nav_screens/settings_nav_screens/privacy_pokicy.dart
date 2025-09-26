c00b3383f1605731d14e47d9c685b9ba931c5e5b
Arslan Qazi <arslanqazi1999@gmail.com>
2025-09-26 09:33:55 +0500

    Update privacy_pokicy.dart

--- lib/src/screen/bottom_nav_screens/settings_nav_screens/privacy_pokicy.dart
+++ lib/src/screen/bottom_nav_screens/settings_nav_screens/privacy_pokicy.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:loneliness/src/components/app_colors_images/app_colors.dart';
 import 'package:loneliness/src/components/common_widget/black_text.dart';
 import 'package:loneliness/src/components/common_widget/custom_back_button.dart';
 
@@ -33,6 +34,43 @@ class PrivacyPolicy extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
             SizedBox(height: screenHeight*.03),
+
+              BlackText(
+                text: "Cancelation Policy",
+                fontSize: 15,
+                fontWeight: FontWeight.w500,
+                textAlign: TextAlign.start,
+              ),
+              SizedBox(height: screenHeight*.02),
+              BlackText(
+                text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n\n"
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.",
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                textAlign: TextAlign.start,
+                textColor: AppColors.greyColor,
+              ),
+              SizedBox(height: screenHeight*.03),
+              BlackText(
+                text: "Terms & Condition",
+                fontSize: 15,
+                fontWeight: FontWeight.w500,
+                textAlign: TextAlign.start,
+              ),
+              SizedBox(height: screenHeight*.02),
+              BlackText(
+                text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n\n"
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n\n"
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.\n\n"
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n\n"
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n\n"
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.",
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                textAlign: TextAlign.start,
+                textColor: AppColors.greyColor,
+              ),
+              SizedBox(height: screenHeight*.05),
 
           ],),
         ),
